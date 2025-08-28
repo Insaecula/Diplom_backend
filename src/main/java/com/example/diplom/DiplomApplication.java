96276134e9cfbd19f670efb2a412079fbaf80b0d
insaecula <morozzzik228@gmail.com>
2025-08-28 23:58:05 +0300

    DTO

--- src/main/java/com/example/diplom/DiplomApplication.java
+++ src/main/java/com/example/diplom/DiplomApplication.java
@@ -2,12 +2,9 @@ package com.example.diplom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DiplomApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiplomApplication.class, args);
 	}
-
 }
